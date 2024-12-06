58bd9c671a6dc88a0734651ed01cc073fa8213d1
marwan <mar1wan08645.gmail.com>
2024-12-06 22:09:54 +0100

    vertox

--- hashcode.go
+++ hashcode.go
@@ -1,17 +1,16 @@
 package piscine
 
-
-//func HashCode(dec string) string {
-//	var vaotrix string = ""
-//	for i := 0 ; i <= len(dec);i++{
-//		hash_code := int(dec+len(dec))%127
-//		if hash_code < 32{
-//			hash_code += 33
-//		}
-//		vaotrix += string(rune(hash_code))
-//	}
-//	return vaotrix
-//}
+func HashCode(dec string) string {
+	var vaotrix string = ""
+	for i := 0; i <= len(dec); i++ {
+		hash_code := int(i+len(dec)) % 127
+		if hash_code < 32 {
+			hash_code += 33
+		}
+		vaotrix += string(rune(hash_code))
+	}
+	return vaotrix
+}
 
 //for i := len(dec) + 1 ; i > 0 ; i++ {
 //if dec[i] >= 'A' && dec[i] <= 'z' {
