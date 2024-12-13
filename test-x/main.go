403077ce917d9f5a8692cf6b7d520740cfb4728f
marwan <mar1wan08645.gmail.com>
2024-12-13 08:31:08 +0100

    vortex

---
+++ test-x/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	piscine "m-m"
+)
+
+func main() {
+	//fmt.Println(piscine.Sum(10, 10))
+	//fmt.Println(piscine.Devison(7, 12))
+	//fmt.Println(piscine.Mains(110, 60))
+	//fmt.Println(piscine.Multipltion(9, 9))
+	fmt.Println(piscine.Lowest(2,1))
+}
